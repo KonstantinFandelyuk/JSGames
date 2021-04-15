35927af84eec1ee61e05faa4dad2aaa9d7ac9bec
Konstantin <fandeluk.k@gmail.com>
2021-04-15 16:01:11 +0300

    add class

---  TicTacToe/app.js
+++  TicTacToe/app.js
@@ -1,83 +1,93 @@
-sessionStorage.clear('');
-const root = document.querySelector(".root");
-const initialState = {
-  winWin: [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7],
-  ],
-  arrayX: [],
-  arrayZerro: [],
-};
+const winWin = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
 
-const createElement = {
+class TikTakToe {
+  arrayX = [];
+  arrayZerro = [];
+  currentFigure = '';
+  startGame = false;
+  constructor(elem) {
+    this.elem = document.querySelector(elem);
+    this.elem.addEventListener('click', (e) => {
+      this.renderFigure(e);
+    });
+  }
+
+  doYouHaveStartGame() {
+    const qGame = confirm('Хотите начать игру?');
+    console.log('object :>> ', qGame);
+    if (qGame) {
+      this.startGame = true;
+    }
+    this.createTable();
+  }
+
   createTable() {
-    for (let i = 9; i > 0; i--) {
-      root.insertAdjacentHTML('afterbegin', `<div class="card card-${i}" data-n=${i}></div>`);
-    }
-  },
-  createSymbol(e, value, random) {
-    if (value === 'o') {
-      random.textContent = value;
+    if (this.startGame) {
+      for (let i = 9; i > 0; i--) {
+        this.elem.insertAdjacentHTML(
+          'afterbegin',
+          `<div class="card card-${i}" data-n=${i}></div>`,
+        );
+      }
     } else {
-      e.target.textContent = value;
+      this.elem.insertAdjacentHTML(
+        'afterbegin',
+        `<div class="">
+      <h1>Начнем игру?</h1>
+      <button type="button" onclick="${this.startGame = true}">Да</button>
+      </div>`,
+      );
     }
-  },
-};
-
-const checkWhoWin = (n, array, text) => {
-  const sortWin = array.sort((a, b) => a - b);
-  const arrayWins = initialState.winWin.filter((item) => item.includes(n));
-  const res = arrayWins
-    .filter((arr) => sortWin.reduce((acc, el) => (arr.includes(el) ? ++acc : acc), 0) === 3)
-    .flat();
-  res.length > 1 ? alert(`${text} выграл`) : null;
-};
-
-const addData = (n, value) => {
-  if (value === 'x') {
-    initialState.arrayX.push(n);
-    checkWhoWin(n, initialState.arrayX, 'Крестик');
   }
-  if (value === 'o') {
-    initialState.arrayZerro.push(n);
-    checkWhoWin(n, initialState.arrayZerro, 'Нолик');
+
+  createSymbol(e, value) {
+    e.target.textContent = value;
   }
-};
 
-root.addEventListener('xEvent', (event) => {
-  const randomNumber =
-    event.detail.emptryArray[Math.floor(Math.random() * event.detail.emptryArray.length)];
-  sessionStorage.setItem('figure', 'o');
-  addData(+randomNumber.dataset.n, 'o');
-  createElement.createSymbol(event, 'o', randomNumber);
-});
-
-const renderFigure = (e) => {
-  let currentFigure = sessionStorage.getItem('figure') ? sessionStorage.getItem('figure', '') : '';
-  if (e.target.textContent) return;
-  if (currentFigure === 'o' || currentFigure === '') {
-    sessionStorage.setItem('figure', 'x');
-    addData(+e.target.dataset.n, 'x');
-    createElement.createSymbol(e, 'x');
+  renderFigure(e) {
+    if (this.startGame) {
+      if (e.target.textContent) return;
+      if (this.currentFigure === 'o' || this.currentFigure === '') {
+        this.currentFigure = 'x';
+        // addData(+e.target.dataset.n, 'x');
+        this.createSymbol(e, 'x');
+      } else {
+        this.currentFigure = 'o';
+        // addData(+e.target.dataset.n, 'o');
+        this.createSymbol(e, 'o');
+      }
+    }
   }
-  const newEvent = new CustomEvent('xEvent', {
-    detail: {
-      figure: 'o',
-      event: e,
-      emptryArray: [...document.querySelectorAll('.card')].filter(
-        (item) => item.textContent === '',
-      ),
-    },
-  });
-  root.dispatchEvent(newEvent);
-};
+}
 
-//start game
-createElement.createTable();
-root.addEventListener('click', renderFigure);
+// const checkWhoWin = (n, array, text) => {
+//   const sortWin = array.sort((a, b) => a - b);
+//   const arrayWins = winWin.filter((item) => item.includes(n));
+//   const res = arrayWins
+//     .filter((arr) => sortWin.reduce((acc, el) => (arr.includes(el) ? ++acc : acc), 0) === 3)
+//     .flat();
+//   res.length > 1 ? alert(`${text} выграл`) : null;
+// };
+
+// const addData = (n, value) => {
+//   if (value === 'x') {
+//     arrayX.push(n);
+//     checkWhoWin(n, arrayX, 'Крестик');
+//   }
+//   if (value === 'o') {
+//     arrayZerro.push(n);
+//     checkWhoWin(n, arrayZerro, 'Нолик');
+//   }
+// };
+
+const firstGame = new TikTakToe('.root');
+firstGame.doYouHaveStartGame();
